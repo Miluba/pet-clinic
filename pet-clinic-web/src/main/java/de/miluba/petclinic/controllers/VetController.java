c848887236c31ebefbfd33e9f3f54a2c53bf1985
Michael Bauer <miluba154@gmail.com>
2018-08-01 13:33:02 +0200

    implemented display list of vets. close #16

--- pet-clinic-web/src/main/java/de/miluba/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/de/miluba/petclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package de.miluba.petclinic.controllers;
 
+import de.miluba.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vet")
 @Controller
 public class VetController {
 
-    @RequestMapping({"","/","/index","/index.html"})
-    public String listVets(){
+    private final VetService vetService;
+
+    public VetController(final VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets(final Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
