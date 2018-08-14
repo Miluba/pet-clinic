935c47bd6a45d6dd8ba8db51498974202e9554b9
Michael Bauer <miluba154@gmail.com>
2018-08-14 14:09:21 +0200

    add Spring Data JPA Vet Service

---
+++ pet-clinic-data/src/main/java/de/miluba/petclinic/services/jpa/VetJPAService.java
@@ -0,0 +1,49 @@
+package de.miluba.petclinic.services.jpa;
+
+import de.miluba.petclinic.model.Vet;
+import de.miluba.petclinic.repositories.VetRepository;
+import de.miluba.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+
+@Service
+@Profile("jpa")
+public class VetJPAService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJPAService(final VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Collection<Vet> findAll() {
+        Collection<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Optional<Vet> findById(Long id) {
+        return vetRepository.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
